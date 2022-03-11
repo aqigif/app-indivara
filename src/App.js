92ceb9116f302d6e554e1a364245ff835862c817
Aqil Gifari <aqilgifari0@gmail.com>
2022-03-11 13:44:38 +0700

    fix unused var

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Peoples from "./pages/Peoples";
 import Todo from "./pages/Todos";
